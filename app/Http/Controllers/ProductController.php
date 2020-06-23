6441bab2bcf7bd74d5c48b9d95c9a7cf7d85a9f0
hafizal hussin <muhamadhafizalhussin@gmail.com>
2020-06-23 12:15:42 +0800

    fix:image user

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -212,8 +212,8 @@ class ProductController extends Controller
             foreach ($user as $users)
             {
 
-            //$json_array = json_decode($product->product_image, true);
-            $json_array = $product->product_image;
+            $json_array = json_decode($product->product_image, true);
+            //$json_array = $product->product_image;
             $imageArray = array();
             
                     foreach ($json_array as $pic)
@@ -1538,8 +1538,8 @@ class ProductController extends Controller
         
                             }  
 
-            //$json_array = json_decode($product->product_image, true);
-            $json_array = $product->product_image;
+            $json_array = json_decode($product->product_image, true);
+            //$json_array = $product->product_image;
             $imageArray = array();
             
                     foreach ($json_array as $pic)
